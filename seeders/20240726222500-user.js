09967991a34231363e1f35bc559a86a13ae6914a
aaridhon19 <adamrizqi.arr@gmail.com>
2024-07-27 08:43:14 +0700

    Seeding

--- seeders/20240726222500-user.js
+++ seeders/20240726222500-user.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+const { hashPassword } = require('../helpers/bcrypt')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -12,14 +14,23 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    let data = JSON.parse(fs.readFileSync("./data/user.json", "utf-8"));
+    data.forEach((item) => {
+      delete item.id;
+      item.password = hashPassword(item.password)
+      item.createdAt = new Date();
+      item.updatedAt = new Date();
+    });
+    await queryInterface.bulkInsert('Users', data)
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
--- seeders/20240726222508-travel.js
+++ seeders/20240726222508-travel.js
@@ -1,8 +1,9 @@
 'use strict';
 
+const fs = require('fs');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -12,14 +13,22 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    let data = JSON.parse(fs.readFileSync("./data/travel.json", "utf-8"));
+    data.forEach((item) => {
+      delete item.id;
+      item.createdAt = new Date();
+      item.updatedAt = new Date();
+    });
+    await queryInterface.bulkInsert('Travels', data)
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Travels', null, {});
   }
 };
